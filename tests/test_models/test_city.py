60f46a7630fc39a65c3555e612fd123c881659c0
GikonyoJohn <99613266+GikonyoJohn@users.noreply.github.com>
2022-09-14 22:13:41 +0300

    Update test_city.py

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,39 +1,41 @@
 #!/usr/bin/python3
-"""
-Contains the TestCityDocs classes
-"""
-
-from datetime import datetime
-import inspect
-import models
-from models import city
-from models.base_model import BaseModel
-import pep8
-import unittest
-City = city.City
+""" """
+from tests.test_models.test_base_model import test_basemodel
+from models.city import City
+from models.state import State
 
 
-class TestCityDocs(unittest.TestCase):
-    """Tests to check the documentation and style of City class"""
-    @classmethod
-    def setUpClass(cls):
-        """Set up for the doc tests"""
-        cls.city_f = inspect.getmembers(City, inspect.isfunction)
+class test_City(test_basemodel):
+    """ """
 
-    def test_pep8_conformance_city(self):
-        """Test that models/city.py conforms to PEP8."""
-        pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['models/city.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "City"
+        self.value = City
 
-    def test_pep8_conformance_test_city(self):
-        """Test that tests/test_models/test_city.py conforms to PEP8."""
-        pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['tests/test_models/test_city.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+    def test_state_id(self):
+        """ """
+        state = State(name="California")
+        new = self.value(state_id=state.id)
+        self.assertEqual(type(new.state_id), str)
 
+    def test_name(self):
+        """ """
+        new = self.value(name="California")
+        self.assertEqual(type(new.name), str)
+
+    def test_any1(self):
+        """Test"""
+        pass
+
+    def test_any2(self):
+        """Test"""
+        pass
+
+    def test_any3(self):
+        """Test"""
+        pass
     def test_city_module_docstring(self):
         """Test for the city.py module docstring"""
         self.assertIsNot(city.__doc__, None,
